3b6ae0f31a3eae217ce9548d13fdaf1c80bb16f4
Alejandro Garcia <elserjc@gmail.com>
2020-04-01 16:35:24 +0200

    v0.6.1RC18

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.6.1RC11"
+  #define SHORT_BUILD_VERSION "v0.6.1RC18"
 		
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -50,7 +50,7 @@
    * version was tagged.
    */
 
-  #define STRING_DISTRIBUTION_DATE "2020-03-23"
+  #define STRING_DISTRIBUTION_DATE "2020-04-01"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
@@ -72,7 +72,7 @@
   /**
    * Defines a generic printer name to be output to the LCD after booting Marlin.
    */
-  #define MACHINE_NAME "BCN3D Epsilon"
+  #define MACHINE_NAME "BCN3D Printer"
 
   /**
    * The SOURCE_CODE_URL is the location where users will find the Marlin Source
