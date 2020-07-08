6c960d7962356e189087ca2c0790545fb15bc759
Alejandro Garcia <elserjc@gmail.com>
2020-07-08 10:13:41 +0200

    ST-719 fix for bcn3d mod
    Also, in G290, it reports bed leveling values when it is on sim mode.

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.7.0RC8"
+  #define SHORT_BUILD_VERSION "v0.7.1RC1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -50,7 +50,7 @@
    * version was tagged.
    */
 
-  #define STRING_DISTRIBUTION_DATE "2020-06-18"
+  #define STRING_DISTRIBUTION_DATE "2020-07-08"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
