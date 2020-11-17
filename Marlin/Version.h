dc298c28773bcbdde294cc92c23ea9c2a20915cf
Alejandro Garcia <elserjc@gmail.com>
2020-11-17 19:49:45 +0100

    Auto bed leveling

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.9.0RC1"
+  #define SHORT_BUILD_VERSION "v0.9.0RC2"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -50,7 +50,7 @@
    * version was tagged.
    */
 
-  #define STRING_DISTRIBUTION_DATE "2020-11-06"
+  #define STRING_DISTRIBUTION_DATE "2020-11-17"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
