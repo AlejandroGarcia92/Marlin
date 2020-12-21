0df9099daa52b13066e0b9ab0deb4269268939c7
Marc Bermejo Tarragó <marcbermejotarrago@gmail.com>
2020-12-21 10:49:04 +0100

    Update distribution date

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -50,7 +50,7 @@
    * version was tagged.
    */
 
-  #define STRING_DISTRIBUTION_DATE "2020-11-24"
+  #define STRING_DISTRIBUTION_DATE "2020-12-21"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
