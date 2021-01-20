e3d70ba38685031091d0318e4954c717482bf862
Alejandro Garcia <elserjc@gmail.com>
2021-01-20 14:12:15 +0100

    save

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.9.0RC3"
+  #define SHORT_BUILD_VERSION "v0.9.0RC4"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -50,7 +50,7 @@
    * version was tagged.
    */
 
-  #define STRING_DISTRIBUTION_DATE "2020-11-26"
+  #define STRING_DISTRIBUTION_DATE "2020-12-03"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
