b5faebe2f2a4606c5dda37b5cd59d70c99cd6eb9
Alejandro Garcia <elserjc@gmail.com>
2021-01-26 11:24:12 +0100

    Release v0.9.1RC1

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.9.0RC6"
+  #define SHORT_BUILD_VERSION "v0.9.1RC1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
