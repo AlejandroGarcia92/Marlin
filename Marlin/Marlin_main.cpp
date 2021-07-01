2e30eea0d618c8140e8e2cf4486e6b5fd096007f
Javier97sm <76102269+Javier97sm@users.noreply.github.com>
2021-07-01 12:39:01 +0200

    Update Marlin_main.cpp

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -12970,7 +12970,7 @@ inline void gcode_M226() {
 	*/
    inline void gcode_M279() {
 	   if (parser.seen('P')) {
-        hasPiezo = false;//parser.boolval('P');
+        hasPiezo = parser.boolval('P');
         SERIAL_ECHOLNPAIR("Machine has piezo: ", hasPiezo);
      }
    }
