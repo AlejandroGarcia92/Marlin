e219327ac4f1f458c4d8eb0c9b29c8a8500ee67e
Javier97sm <76102269+Javier97sm@users.noreply.github.com>
2021-07-01 12:40:53 +0200

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.10.0RC13"
+  #define SHORT_BUILD_VERSION "v0.10.0RC14"
 
   /**
    * Verbose version identifier which should contain a reference to the location
