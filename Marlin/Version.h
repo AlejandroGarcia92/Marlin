5bbfa87a91a15acafd24356f518e62d5199f5811
monell1997 <55438983+monell1997@users.noreply.github.com>
2021-07-08 15:45:46 +0200

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.11.0RC5"
+  #define SHORT_BUILD_VERSION "v0.11.0RC6"
 
   /**
    * Verbose version identifier which should contain a reference to the location
