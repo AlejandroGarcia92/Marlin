f04ba11891f7e1da874cce97ba415869dd546b73
monell1997 <55438983+monell1997@users.noreply.github.com>
2021-07-14 12:20:40 +0200

    save

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "v0.11.0RC9"
+  #define SHORT_BUILD_VERSION "v0.11.0RC10"
 
   /**
    * Verbose version identifier which should contain a reference to the location
